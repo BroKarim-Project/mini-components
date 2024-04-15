ffb37d43d297f0731cd72094d1ed74377067f3a6
BroKarim <146515339+BroKarim@users.noreply.github.com>
2024-04-15 15:09:04 +0700

    Update SplitText.jsx

--- src/components/SplitText/SplitText.jsx
+++ src/components/SplitText/SplitText.jsx
@@ -2,9 +2,8 @@ import React, { useState } from 'react';
 import './text-hover';
 
 const SplitText = () => {
-  const [splitType, setSplitType] = useState('letter'); // State untuk menyimpan jenis pembagian saat ini
+  const [splitType, setSplitType] = useState('letter'); 
   const [activeButton, setActiveButton] = useState(null);
-  // Fungsi untuk mengubah jenis pembagian saat tombol ditekan
   const handleSplitChange = (type) => {
     setSplitType(type);
     setActiveButton(type);
@@ -18,12 +17,7 @@ const SplitText = () => {
           <button className={activeButton === 'letter' ? 'p-2 bg-blue-400 rounded-md text-white hover:bg-black' : 'p-2 bg-black rounded-md text-white hover:bg-blue-400'} onClick={() => handleSplitChange('letter')}>
             Type 1
           </button>
-          {/* <button className={activeButton === 'word' ? 'p-2 bg-blue-400 rounded-md text-white hover:bg-black' : 'p-2 bg-black rounded-md text-white hover:bg-blue-400'} onClick={() => handleSplitChange('word')}>
-            Type 2
-          </button> */}
         </div>
-
-        {/* split 1 */}
         <div>
           {splitType === 'letter' && (
             <text-hover split-by="letter" letter-animation>
@@ -43,22 +37,3 @@ const SplitText = () => {
 };
 
 export default SplitText;
-
-/*
-Penjelasan : 
-- Keknya sekarang fokus ke split text dulu sampe bberapa animasi, min.5 lah
-- Untuk design template ikutin @sona_code
-
-- Keknya kedepan untuk memudahka cari ide + pemahaman, untuk setiap topik kita bakal buat min.2 variasi
-
-
-Sumber : 
- # code
-- https://web.dev/articles/building/split-text-animations?hl=id
- # pembahasan
-- https://web.dev/articles/building/split-text-animations
-
-Pemicu : 
-- https://codepen.io/GreenSock/pen/pEKYVz
-
-*/
